fe3984c775e50711e96de18821bf1d4bffb4629c
SdRaro <132179472+SdRaro@users.noreply.github.com>
2023-04-29 22:07:27 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Raro#6870'
+description 'A sentence in a short command and displays in 3d text'
+repository ''
+
+client_scripts {
+    'client.lua'
+}
+
+server_script 'server.lua'
